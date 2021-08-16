29f1e9ff03f92523d1379456be9641988bcd58bc
typed <gxj21720@126.com>
2021-08-17 00:20:06 +0800

    1

--- makeuci.py
+++ makeuci.py
@@ -2,10 +2,10 @@ import os
 for root,dirs,files in os.walk(r".\img"):
     for file in files:
         src = os.path.join(root, file)
-        des = src.replace("img", "img_hci")
+        des = src.replace("img", "img_uci")
         des_dir = os.path.dirname(des)
         if not os.path.exists(des_dir):
             os.makedirs(os.path.dirname(des))
-        cmd = "imgdec {0} - | ucienc - -o \"{1}.hci\" -q 10 -Q 10".format(src,des)
+        cmd = "imgdec {0} - | ucienc - -o \"{1}.uci\" -q 10 -Q 10".format(src,des)
         #print(cmd)
         os.system(cmd)
